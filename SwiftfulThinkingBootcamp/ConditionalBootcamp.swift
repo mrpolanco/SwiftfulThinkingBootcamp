6cd6c47b0ef454927ed6caec46f5b296952eb7ad
Rich Polanco <rich.polanco@automattic.com>
2025-01-24 14:51:32 -0600

    Conditional bootcamp

---
+++ SwiftfulThinkingBootcamp/ConditionalBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ConditionalBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Rich Polanco on 1/24/25.
+//
+
+import SwiftUI
+
+struct ConditionalBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConditionalBootcamp()
+}
